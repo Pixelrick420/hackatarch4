4a13eb2eaed6726c0f09cd5fac9815aa9f56edc2
Harikrishnan R <harikrishnanr0505@gmail.com>
2026-01-17 18:12:04 +0530

    add fluid cursor

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import './App.css';
 import About from './Components/About';
-import Clubs from './Components/Clubs';
 import CommunityPartners from './Components/CommunityPartners';
 import ScrollingDivider from './Components/Divider';
 import Hero from './Components/Hero';
+import FluidCursor from './Components/Mouse';
 import Sponsors from './Components/Sponsors';
 import Timer from './Components/Timer';
 import Workshops from './Components/WorkShops';
@@ -11,14 +11,15 @@ import Workshops from './Components/WorkShops';
 function App() {
     return (
         <>
-            <Hero />
-            <ScrollingDivider />
-            <About />
-            <Clubs />
-            <Timer />
-            <Workshops />
-            <Sponsors />
-            <CommunityPartners />
+            <FluidCursor color="#3AAE95" opacity={0.2}>
+                <Hero />
+                <ScrollingDivider />
+                <About />
+                <Timer />
+                <Workshops />
+                <Sponsors />
+                <CommunityPartners />
+            </FluidCursor>
         </>
     );
 }
